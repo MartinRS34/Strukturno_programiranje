8da686a8c4b3922bb0cf46aa3a4d9087719bbe60
MartinRS34 <118695648+MartinRS34@users.noreply.github.com>
2024-01-04 19:11:42 +0100

    init

--- Auditoriska_vezba_6/zad1.cpp
+++ Auditoriska_vezba_6/zad1.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
-float diametar(int d){
-    return 2*d;
+float diametar(int d) {
+    return 2 * d;
 }
 
-float perimetar(int p){
-    return 2*p*3.14;
+float perimetar(int p) {
+    return 2 * p * 3.14;
 }
 
-float plostina(int pp){
-    return pp*pp*3.14;
+float plostina(int pp) {
+    return pp * pp * 3.14;
 }
 
-int main(){
+int main() {
     int radius;
-    cin>>radius;
-    float D= diametar(radius);
-    float L= perimetar(radius);
-    float P= plostina(radius);
+    cin >> radius;
+    float D = diametar(radius);
+    float L = perimetar(radius);
+    float P = plostina(radius);
 
-    cout<<"Diametar: "<<D<<endl<<"Perimetar: "<<L<<endl<<"Plostina: "<<P;
+    cout << "Diametar: " << D << endl << "Perimetar: " << L << endl << "Plostina: " << P;
 }
